237b59c9b5ece0f08ada468a7c7fad31561e5c5a
ZhuWuyin <70063950+ZhuWuyin@users.noreply.github.com>
2021-09-22 11:26:30 +0800

    Delete limit directory

--- limit/lim.py
+++
@@ -1,8 +0,0 @@
-a=eval(input("Please input limit value: "))
-p=int(input("Please input precision: "))
-f=input("please input function: y=")
-for i in range(p):
-    x=eval(str(a-1)+"."+"9"*(i+1))
-    print(eval(f),end="\tlimit\t")
-    x=eval(str(a+1)+"."+"0"*(i+1)+"1")
-    print(eval(f))
