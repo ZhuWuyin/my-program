a4f495f75d58b6ea2881e495c011c4ba082001d8
ZhuWuyin <70063950+ZhuWuyin@users.noreply.github.com>
2022-02-11 23:58:46 +0800

    Add files via upload

---
+++ ImageCompression/ImageCompression.py
@@ -0,0 +1,107 @@
+from typing import Counter
+import numpy as np
+import cv2
+
+def avg(list1):
+    return sum(list1)/len(list1)
+
+def approximate(tolerance, list1):
+    maxCounter=0
+    maxList=[]
+
+    counteri=0
+    for i in list1:
+        avgList=[]
+
+        counteri+=1
+        counterj=counteri
+        counter=1
+        for j in list1[counteri:]:
+
+            interim=[]
+            for k in range(len(i)):
+                if i[k]-tolerance<j[k] and i[k]+tolerance>j[k]:
+                    interim.append(j[k])
+
+            if len(interim)==len(i):
+                avgList.append(list1.pop(counterj))
+                counter+=1
+            else :
+                counterj+=1
+
+        if avgList:
+            avgList.append(i)
+            pixel=[]
+            for num in range(len(i)):
+                pixel.append(int(avg([val[num] for val in avgList])))
+            if maxCounter<counter:
+                maxCounter=counter
+                maxList=pixel
+
+    return maxList
+
+def normalModel(list1, tolerance):
+    pixelContainer=approximate(tolerance, list1)
+    if pixelContainer:
+        return pixelContainer
+    else :
+        return [int(avg([j[i] for j in list1])) for i in range(len(list1[0]))]
+
+def compressPixel(pixel, tolerance, model):
+    list1=[k for i in pixel for k in i]
+
+    if model=="sharp":
+        if len(list1)==len(set([str(i) for i in list1])):
+            return normalModel(list1, tolerance)
+        else :
+            return eval(list(Counter([str(i) for i in list1]))[0])
+    elif model=="normal" :
+        return normalModel(list1, tolerance)
+    else :
+        return [int(avg([j[i] for j in list1])) for i in range(len(list1[0]))]
+
+def getPixel(xShift, yShift, pixelWidth, pixelHeight, img):
+    pixel=[]
+
+    height=yShift+pixelHeight
+    imgHeight=len(img)
+
+    width=xShift+pixelWidth
+    imgWidth=len(img[0])
+
+    if height>imgHeight:
+        pixelHeight-=height-imgHeight
+    if width>imgWidth:
+        pixelWidth-=width-imgWidth
+
+    for i in range(yShift, yShift+pixelHeight):
+        line=[]
+        for j in range(xShift, xShift+pixelWidth):
+            line.append(img[i][j])
+        pixel.append(line)
+
+    return pixel
+
+imgPath=input("Image path: ").replace("\\", "/")
+outputName=input("Output image's name: ")+imgPath[imgPath.index("."):]
+im1=cv2.imread(imgPath).tolist()
+
+indexWidth=int(input("Width index: "))
+indexHeight=int(input("Height index: "))
+tolerance=int(input("Tolerance: "))
+
+while True:
+    model=input("Process model(normal/sharp/quick): ").lower()
+    if model=="normal" or model=="sharp" or model=="quick":
+        break
+    print("No such model")
+
+image=[]
+for i in range(0, len(im1), indexHeight):
+    line=[]
+    for j in range(0, len(im1[0]), indexWidth):
+        line.append(compressPixel(getPixel(j, i, indexWidth, indexHeight, im1), tolerance, model))
+
+    image.append(line)
+
+cv2.imwrite(outputName, np.array(image, dtype="uint8"))
