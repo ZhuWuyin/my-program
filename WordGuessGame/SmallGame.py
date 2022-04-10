49fd413b90e1500163cb7ad9f68ec41aa5ec3357
ZhuWuyin <70063950+ZhuWuyin@users.noreply.github.com>
2022-04-10 17:46:09 +0800

    Update SmallGame.py

--- WordGuessGame/SmallGame.py
+++ WordGuessGame/SmallGame.py
@@ -1,3 +1,4 @@
+# Ugly program
 alphabet=[["A", "B", "C", "D", "E"], ["F", "G", "H", "I", "J"], ["K", "L", "M", "N", "O"], ["P", "Q", "R", "S", "T"], ["U", "V", "W", "X", "Y"], ["Z", "", "", "", ""]]
 print('\n'.join([', '.join(k) for k in [[j for j in i if j] for i in alphabet]]))
 newAlphabet=[[l[k] for l in alphabet] for k in [int(i) for i in input("\nPlease input the colum number from zero\n").split()]]
